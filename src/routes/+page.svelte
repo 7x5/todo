48ef380d11d18961e57ebe7b2275a0e0f6f23bba
7x5 <jacob.reinerholm@me.com>
2023-04-28 23:25:47 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { each } from "svelte/internal";
+
+    let todo = [];
+    let item = '';
+    function addItem() {
+        todo = todo.concat(item);
+    }
+</script>
+<h1>TODO</h1>
+<ul>
+    {#each todo as object}
+    <div style=display:flex;>
+        <button>X</button> <li>{object}</li>
+    </div>
+    {/each}
+    <input bind:value={item}>
+    <button on:click={addItem}>Add Item</button>
+</ul>
