8cc7b050a55b78183e52ffe65c69fa8c1f3894c5
7x5 <jacob.reinerholm@me.com>
2023-05-01 17:26:49 +0200

    added delete + css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,60 @@
 <script>
-    import { each } from "svelte/internal";
+    import { each, text } from "svelte/internal";
 
-    let todo = [];
-    let item = '';
+    let todo = [
+    { done: false, name: 'Fix the todo app' },
+    { done: false, name: 'CSS' },
+    { done: false, name: 'Present' }
+    ];
+    let item;
     function addItem() {
-        todo = todo.concat(item);
+        todo = todo.concat({ done: false, name: '' });
+        console.log(todo)
+    }
+    function removeItem() {
+        todo = todo.filter(i => !i.done)
     }
 </script>
 <h1>TODO</h1>
 <ul>
     {#each todo as object}
     <div style=display:flex;>
-        <button>X</button> <li>{object}</li>
+        <button class="btn" on:click="{object.done = !object.done }" on:click="{removeItem}">X</button> <input class="inp" placeholder="Add TODO here" bind:value={object.name}>
     </div>
     {/each}
-    <input bind:value={item}>
-    <button on:click={addItem}>Add Item</button>
+    <br>
+    <button class="submit" on:click={addItem}>Add Item</button>
 </ul>
+
+<style>
+    * {
+        font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+    }
+
+    .btn {
+        margin-bottom: 5px;
+        margin-right: 5px;
+        background-color: red;
+        border: 0px;
+        border-radius: 5px;
+        box-shadow:  3px 3px 0px -1px rgba(0, 0, 0, 0.81);
+-webkit-box-shadow:  3px 3px 0px -1px rgba(0, 0, 0, 0.81);
+    }
+
+    .inp {
+        border-radius: 5px;
+        margin-bottom: 5px;
+        margin-right: 2px;
+        border: solid 1px black;
+        box-shadow:  2px 2px 0px -1px rgba(0, 0, 0, 0.81);
+-webkit-box-shadow:  2px 2px 0px -1px rgba(0, 0, 0, 0.81);
+    }
+
+    .submit {
+        background-color: #00cc00;
+        border: 0px;
+        border-radius: 5px;
+        box-shadow:  3px 3px 0px -1px rgba(0, 0, 0, 0.81);
+-webkit-box-shadow:  3px 3px 0px -1px rgba(0, 0, 0, 0.81);
+    }
+</style>
